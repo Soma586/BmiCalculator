306afd4b666f40b170203706977d53e3283fe2fe
Sabo <sabo@Sabos-MBP.home>
2023-06-09 01:50:27 -0400

    computed full layout for person section

--- src/components/Person/index.jsx
+++ src/components/Person/index.jsx
@@ -1,5 +1,6 @@
 import Man from '../../assets/images/image-man-eating.webp'
 import './styles.scss'
+import LeftCurve from '../../assets/images/pattern-curved-line-left.svg'
 
 const Person = () => {
 
@@ -7,12 +8,14 @@ const Person = () => {
     return(
         <div className="container personContainer">
 
-            <div className="d-flex justify-space-between">
+            <div className="d-md-flex justify-content-between">
 
                 <img src={Man} className="wtf"/>
 
                 <div className="bmiDescription">
-                    <h2 className="type__HeadingL">What your BMI result means</h2>
+
+                    <img  className="personCurve" src={LeftCurve}/>
+                    <h2 className="type__HeadingL personTitle">What your BMI result means</h2>
 
                     <p className="type__BodyM mt-5">
                     A BMI range of 18.5 to 24.9 is considered a 'healthy weight.' Maintaining a healthy weight may lower your chances of experiencing health issues later on, such as obesity and type 2 diabetes. Aim for a nutritious diet with reduced fat and sugar content, incorporating ample fruits and vegetables. Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a week.
