02947e52b48e9c088f0a0d04e68e482e7a3aa535
Sabo <sabo@Sabos-MBP.home>
2023-06-12 17:44:44 -0400

    I have the full layout of the page about 90% completed

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -5,22 +5,24 @@ import Logo from '../../assets/images/logo.svg'
 const Hero = () => {
 
     return (
-        <div className="container heroContainer">
-            <div className="row h-100">
-                <div className="col-sm-8 leftSide ">
+        <div className="container heroContainer d-lg-flex justify-content-between">
+           
+                <div className="leftSide ">
                     <img className="logo" src={Logo}/>
 
                     <div className="heroDesc">
-                    <h1 className="type__HeadingXL">Body Mass Index Calculator</h1>
+                    <h1 className="type__HeadingXL heroTitle">Body Mass Index Calculator</h1>
 
                     <p className="type__BodyM">Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.</p>
                     </div>
                 </div>
-                <div className="col-sm-4 rightSide">
+                <div className="rightSide">
                     <Calculator/>
                 </div>
 
-            </div>
+            
+
+            {/* <Calculator/> */}
 
         </div>
     )
--- src/components/calculator/index.jsx
+++ src/components/calculator/index.jsx
@@ -1,30 +1,77 @@
 import "./styles.scss";
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect } from "react";
+
+//const map = new Map()
+
+//map.get('147')
+
+const cm = [
+  "193",
+  "191",
+  "188",
+  "185",
+  "183",
+  "180",
+  "178",
+  "175",
+  "172",
+  "170",
+  "168",
+  "165",
+  "163",
+  "160",
+  "157",
+  "155",
+  "152",
+  "150",
+  "147",
+];
+
+const normalWeight = [
+  "71 - 89kgs",
+  "69 - 87kgs",
+  "67 - 84kgs",
+  "65 - 83kgs",
+  "64 - 80kgs",
+  "62 - 78kgs",
+  "60 - 76kgs",
+  "58 - 73kgs",
+  "57 - 72kgs",
+  "55 - 69kgs",
+  "54 - 67kgs",
+  "52 - 65kgs",
+  "50 - 64kgs",
+  "49 - 61kgs",
+  "47 - 59kgs",
+  "45 - 57kgs",
+  "44 - 56kgs",
+  "43 - 54kgs",
+  "41 - 52kgs",
+];
 
 const Calculator = () => {
+  const [height, setHeight] = useState("");
 
+  const [weight, setWeight] = useState("");
 
-
-  
-
-
-  const [height, setHeight] = useState("")
-
-  const [weight, setWeight] = useState("")
-
-  const [metricTotal, setMetricTotal] = useState(1)
+  const [metricTotal, setMetricTotal] = useState(1);
 
   //const [isImperial, setImperial] = useState(false)
 
-  const [isMetetic, setMetric] = useState(true)
+  const [isMetetic, setMetric] = useState(true);
 
-  const [feet, setFeet] = useState(0)
-  const [inches, setInches] = useState('')
-  const [imperialWeight, setImperialWeight] = useState('')
+ 
 
-  const [inputValues, setInputValues] = useState([''])
+  const inputRefs = useRef([]);
+  //const inputRef = useRef(null)
 
-  const inputRefs = useRef([])
+  const [inputValues, setInputValues] = useState(["", "", "", "", ""]);
+
+
+
+  const [currentIndex, setIndex] = useState(0);
+  const [healthStatus, setHealthStatus] = useState("");
+  const [weightRatio, setWeightRatio] = useState("");
 
   
 
@@ -32,262 +79,221 @@ const Calculator = () => {
     const newInputValues = [...inputValues];
     newInputValues[index] = event.target.value;
 
-    // console.log(newInputValues)
-    // console.log(event.value.target)
+   
+    if (index === 0) {
+      handleHeight(event);
+    } else {
+      handleWeight(event);
+    }
+    setIndex(index);
     setInputValues(newInputValues);
+    postResult();
+    idealWeight();
   };
 
   useEffect(() => {
-    const lastIndex = inputRefs.current.length - 1;
-    if (inputRefs.current[lastIndex]) {
-      inputRefs.current[lastIndex].focus();
+   
+    if (inputRefs.current[currentIndex]) {
+      inputRefs.current[currentIndex].focus();
     }
   }, [inputValues]);
 
+  
+  const idealWeight = () => {
+    if (height >= 200 || height <= 140) {
+      setWeightRatio("unknown");
+      return;
+    }
 
-  const setM = () => {
-    console.log("Metric")
-    setMetric(true)
-  }
-
-  const setImp = () => {
-    console.log("Imperial")
-    setMetric(false)
-  }
-
+    for (let i = 0; i < cm.length; i++) {
+      if (height >= cm[i]) {
+        setWeightRatio(normalWeight[i]);
+        return;
+      }
+    }
+  };
 
   const handleHeight = (event) => {
+  
 
-    console.log(event.target.value)
-
-    //console.log(typeof +height)
-
-    setHeight(event.target.value)
-    //debugger
-
-    // if(weight !== ""){
-    //   //const total = (+weight/Math.pow(+height, 2))
-
-    //   // console.log(`hey ${total}`)
-
-    //   const h = +event.target.value
-    //   const w = +weight
-
-    //   const rawtotal = (w/Math.pow(h, 2)) * 10000 
-
-    //   const total = Math.round(rawtotal * 10) /10
-
-    //   console.log(`${h}`)
-    //   console.log(`${w}`)
-    //   console.log(total)
-
-    //   // //console.log(total)
-    //   setMetricTotal(total)
-    //   // console.log(`wtf ${metricTotal}`)
-
-    // }
-
-    
+    setHeight(event.target.value);
     
 
-  }
+    if (weight !== "") {
+  
 
+      const h = +event.target.value;
+      const w = +weight;
 
+      const rawtotal = (w / Math.pow(h, 2)) * 10000;
+
+      const total = Math.round(rawtotal * 10) / 10;
+
+    
+      setMetricTotal(total);
+    }
+  };
+
+  const postResult = () => {
+    if (metricTotal < 18.5) {
+      setHealthStatus("you're underweight");
+    } else if (metricTotal >= 18.5 && metricTotal <= 24.9) {
+      setHealthStatus("you're a healthy weight");
+    } else {
+      setHealthStatus("you're overweight");
+    }
+  };
 
   const handleWeight = (event) => {
+    setWeight(event.target.value);
 
-    setWeight(event.target.value)
+    if (height !== "") {
+      const h = +height;
+      const w = +event.target.value;
 
-    if(height !== ""){
+      const rawtotal = (w / Math.pow(h, 2)) * 10000;
 
-      const h = +height
-      const w = +event.target.value
+      const total = Math.round(rawtotal * 10) / 10;
 
-      const rawtotal = (w/Math.pow(h, 2)) * 10000 
-
-      const total = Math.round(rawtotal * 10) /10
-
-      setMetricTotal(total)
-
-      
-
+      setMetricTotal(total);
     }
-    
-    //setWeight(event.target.value)
-  }
 
-  const imperialHeight = (event) => {
+   
+  };
 
-    setFeet(+event.target.value)
-    console.log(event.target.value)
-
-    // if(feet !== "" && inches !== ""){
-
-    //   const h = (+feet * 12) + 10
-    //   const w = +imperialWeight 
-
-    //   const rawtotal = (w/Math.pow(h, 2)) * 703
-
-    //   const total = Math.round(rawtotal * 10) /10
-
-    //   console.log(total)
-
-
-    // }
-
-
-  }
-
-  const imperialInches = (event) => {
-
-    console.log(event.target.value)
-    setInches(event.target.value)
-  }
-
-
-  const handleWeightz = (event) => {
-
-    console.log(event.target.value)
-    setImperialWeight(event.target.value)
-
-  }
-
-  const handleFormSubmit = (event) => {
-
-    event.preventDefault()
-  }
-
+  
 
   const ImperialBlock = () => {
+    return (
+      <div>
+        <div className="d-flex justify-content-between">
+          <div>
+            <p className="type__BodyS">Height</p>
+            <input
+              className="type__BodyMBold"
+              ref={(el) => (inputRefs.current[2] = el)}
+              value={inputValues[2]}
+              onChange={(event) => handleInputChange(event, 2)}
+              type="number"
+            />
+
+            <span className="unit type__Headings">ft</span>
+          </div>
+
+          <div>
+            <input
+              className="type__BodyMBold inches"
+              type="number"
+              min="0"
+              max="11"
+              ref={(el) => (inputRefs.current[3] = el)}
+              value={inputValues[3]}
+              onChange={(event) => handleInputChange(event, 3)}
+            />
+            <span className="unit type__Headings">in</span>
+          </div>
+        </div>
+
+        <div className="d-flex justify-content-between">
+          <div>
+            <p className="type__BodyS">Weight</p>
+            <input
+              className="type__BodyMBold"
+              type="number"
+              key={6}
+              ref={(el) => (inputRefs.current[4] = el)}
+              value={inputValues[4]}
+              onChange={(event) => handleInputChange(event, 4)}
+            />
+            <span className="unit type__Headings">lbs</span>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const MetricBlock = () => {
+    const test = inputValues.map((value, index) => {
+      return (
+        <input
+          key={index}
+          type="number"
+          ref={(el) => (inputRefs.current[index] = el)}
+          value={value}
+          onChange={(event) => handleInputChange(event, index)}
+        />
+      );
+    });
 
     return (
+      <div className="d-md-flex ">
+        <div className="heightDivBuffer">
+          <p className="type__BodyS">Height</p>
+          <input
+            className="type__BodyMBold"
+            id="lol"
+            key={0}
+            ref={(el) => (inputRefs.current[0] = el)}
+            value={inputValues[0]}
+            onChange={(event) => handleInputChange(event, 0)}
+            type="number"
+          />
+          <span className="unit type__Headings"> cm</span>
+        </div>
 
-      <div>
-      <div className="d-flex justify-content-between">
-
-      <form onSubmit={handleFormSubmit}>
-      <div>
-      <p className="type__BodyS">Height</p>
-        <input className="type__BodyMBold" type="number" onChange={imperialHeight} value={feet}/>
-        <span className="unit type__Headings">ft</span>
+        <div>
+          <p className="type__BodyS weightBuffer">Weight</p>
+          <input
+            className="type__BodyMBold"
+            key={1}
+            ref={(el) => (inputRefs.current[1] = el)}
+            value={inputValues[1]}
+            onChange={(event) => handleInputChange(event, 1)}
+            type="number"
+          />
+          <span className="unit type__Headings"> kg</span>
+        </div>
       </div>
-
-{/*       
-      <div>
-        <input className="type__BodyMBold inches"  type="number"  min="0"  max="11" onChange={imperialInches}/>
-        <span className="unit type__Headings">in</span>
-      </div>
-      
-      </div>
-
-      <div className="d-flex justify-content-between">
-
-      <div>
-      <p className="type__BodyS">Weight</p>
-      <input className="type__BodyMBold" onChange={handleWeightz} type="number"/>
-        <span className="unit type__Headings">lbs</span>
-      </div> */}
-
-    </form>
-      
-      </div>
-      </div>
-
-    )
-  }
-
-  const MetricBlock = () =>{
-
-    return (
-
-      <div className="d-flex justify-content-between">
-
-
-      <form>
-      <div>
-      <p className="type__BodyS">Height</p>
-        <input className="type__BodyMBold"  
-        ref={(el) => (inputRefs.current[0] = el)} 
-        value={height} 
-        onChange={(event) => handleInputChange(event, 0)} 
-        type="number"/>
-        <span className="unit type__Headings"> cm</span>
-      </div>
-
-      
-      {/* <div>
-      <p className="type__BodyS">Weight</p>
-        <input className="type__BodyMBold" ref={inputRef} value={weight} onChange={handleWeight} type="number"/>
-        <span className="unit type__Headings"> kg</span>
-      </div>
-       */}
-      
-      </form>
-      </div>
-      
-      
-    )
-  }
-
+    );
+  };
 
   return (
     <div className="calContainer">
-      <h4>Enter your details below</h4>
+      <h4 className="calTitle">Enter your details below</h4>
 
 
-     
-      <form className="mt-5 d-flex justify-content-between metircImperial">
+      {isMetetic ? <MetricBlock /> : <ImperialBlock />}
 
-        
-      
-        <div>
-        <input type="radio" id="Metric" checked={isMetetic} name="abc" onChange={setM}/>
-        <label className="ms-3 type__BodyM " for="Metric">Metric</label>
-        </div>
+      <div className="d-flex justify-content-between mt-4">
+        <div className="resultsContainer ">
+          <div className="descLeftSide">
+            {height === "" || weight === "" ? (
+              <div>
+                <h3 className="type__HeadingM"> Welcome!</h3>
+                <p className="type__BodyS">
+                  Enter your Height and weight and you;ll see your BMI result
+                  here
+                </p>
+              </div>
+            ) : (
+              <div>
+                <p className="type__BodyM">Your BMI is...</p>
 
-        <div>
-        <input type="radio" id="Imperial" name="abc" onChange={setImp}/>
-        <label classNames="ms-3 type__BodyM " for="Imperial">Imperial</label>
-        </div>
-      </form>
+                <h2 className="type__HeadingXL">{metricTotal}</h2>
+              </div>
+            )}
+          </div>
 
-    {isMetetic ? 
-      <MetricBlock/>
-
-      :
-      <ImperialBlock/>
-
-    }
-
-<div className="d-flex justify-content-between mt-4">
-      
-
-      
-      
-      
-        
-      
-      
-
-
-      <div className="resultsContainer ">
-        <div className="descLeftSide">
-          {/* <p className="type__BodyM">Your BMI is...</p>
-
-          <h2 className="type__HeadingXL">{metricTotal}</h2> */}
-
-          <h3 className="type__HeadingM"> Welcome!</h3>
-          <p className="type__BodyS">Enter your Height and weight and you;ll see your BMI result here</p>
-        </div>
-
-        <div>
-          {/* <p className="type__BodyS">
-            Your BMI suggests you're a healthy weight. Your ideal wieght is
-            between
-          </p> */}
+          {(height !== "" || weight !== "") && (
+            <div>
+              <p className="type__BodyS mt-3">
+                Your BMI suggests {healthStatus}. Your ideal weight is between{" "}
+                <span className="weightRatio"> {weightRatio}</span>.
+              </p>
+            </div>
+          )}
         </div>
       </div>
-    </div>
     </div>
   );
 };
